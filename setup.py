40c3bd8430adb3c281d03efa4b15bd0d26ecd429
CT_B <fafamans@gmail.com>
2025-10-28 19:05:39 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,37 +13,38 @@ setting = {
         'name': '자동 트레이딩',  # 플러그인 이름
         'list': [
             {
-                'uri': 'core/setting',  # 설정 화면 URI
+                'uri': 'basic/setting',  # 설정 화면 URI
                 'name': '설정',
             },
+            #{
+            #    'uri': 'core/history',  # 거래 히스토리 화면 URI
+            #    'name': '거래 기록',
+            #},
             {
-                'uri': 'core/history',  # 거래 히스토리 화면 URI
-                'name': '거래 기록',
-            },
-            {
-                'uri': 'core/manual',  # 메뉴얼 화면 URI
-                'name': '메뉴얼',
+                'uri': 'manual',
+                'name': '매뉴얼',
                 'list': [
-                    {'uri': 'README.md', 'name': 'README.md'},  # README 파일 링크
+                    {'uri': 'README.md', 'name': 'README.md'},
                 ]
             },
             {
-                'uri': 'core/log',  # 로그 화면 URI
+                'uri': 'log',
                 'name': '로그',
             },
         ]
     },
     'setting_menu': None,
-    'default_route': 'core/setting',  # 기본 화면
+    'default_route': 'normal',
 }
 
-# 플러그인 인스턴스 생성
+from plugin import *
+
 P = create_plugin_instance(setting)
 
-# 모듈 등록
 try:
-    from .mod_core import ModuleCore  # core 모듈을 만들어야 함
-    P.set_module_list([ModuleCore])
+    from .mod_basic import ModuleBasic
+    P.set_module_list([ModuleBasic])
 except Exception as e:
     P.logger.error(f'Exception:{str(e)}')
     P.logger.error(traceback.format_exc())
+
