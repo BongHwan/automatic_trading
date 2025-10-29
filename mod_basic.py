66476cf88ed6d8ffc1de59d01d307f261509e0f0
CT_B <fafamans@gmail.com>
2025-10-29 12:49:05 +0900

    Update mod_basic.py

--- mod_basic.py
+++ mod_basic.py
@@ -1,41 +1,33 @@
 # mod_basic.py
-import traceback
+import traceback, os, time
 from flask import render_template, jsonify
 from plugin import PluginModuleBase
 from .setup import *
-
-import os
-import time
-from io import BytesIO
-
-# Binance 연동 및 메시지 알림 라이브러리 예시
 from .trading_engine import TradingEngine
-from .tool import ToolNotify
+from .notifier import send_telegram_message
 
 class ModuleBasic(PluginModuleBase):
 
     def __init__(self, P):
-        super(ModuleBasic, self).__init__(
-            P, name='basic', first_menu='setting', scheduler_desc="자동 트레이딩"
-        )
-
+        super().__init__(P, name='basic', first_menu='setting', scheduler_desc="자동 트레이딩")
         self.db_default = {
             'db_version': '1',
             f'{self.name}_auto_start': 'False',
-            f'{self.name}_interval': '0 */1 * * *',  # 예시: 1시간마다 실행
-            f'{P.package_name}_trade_amount_percent': 10,
-            f'{P.package_name}_leverage': 1,
-            f'{P.package_name}_take_profit_percent': 1,
-            f'{P.package_name}_stop_loss_percent': 1,
-            f'{P.package_name}_max_holding_minutes': 60,
-            f'{P.package_name}_mode': 'simulation',
-            f'{P.package_name}_telegram_api_key': '',
-            f'{P.package_name}_telegram_chat_id': '',
+            f'{self.name}_interval': '0 */1 * * *',
+            f'{self.name}_trade_amount_percent': 10,
+            f'{self.name}_leverage': 1,
+            f'{self.name}_take_profit_percent': 1,
+            f'{self.name}_stop_loss_percent': 1,
+            f'{self.name}_max_holding_minutes': 60,
+            f'{self.name}_mode': 'simulation',
+            f'{self.name}_telegram_enabled': False,
+            f'{self.name}_telegram_api_key': '',
+            f'{self.name}_telegram_chat_id': '',
         }
 
     def process_menu(self, sub, req):
         arg = P.ModelSetting.to_dict()
-        return render_template(f'{P.package_name}_{sub}.html', arg=arg)
+        return render_template(f'{P.package_name}_{self.name}_{sub}.html', arg=arg)
 
     def process_command(self, command, arg1, arg2, arg3, req):
         ret = {'ret': 'success'}
@@ -54,7 +46,7 @@ class ModuleBasic(PluginModuleBase):
 
     def scheduler_function(self):
         try:
-            auto_start = P.ModelSetting.get_bool('basic_auto_start')
+            auto_start = P.ModelSetting.get_bool(f'{self.name}_auto_start')
             if not auto_start:
                 return
 
@@ -62,16 +54,9 @@ class ModuleBasic(PluginModuleBase):
             result = engine.execute_trade()
             msg = f"자동 트레이딩 실행 결과\n{result}"
 
-            telegram_enabled = P.ModelSetting.get_bool('telegram_enabled')  # bool 형태로 가져오기
-            telegram_key = P.ModelSetting.get('telegram_api_key')
-            telegram_chat = P.ModelSetting.get('telegram_chat_id')
-            if telegram_enabled and telegram_key and telegram_chat:
-                ToolNotify.send_message(msg, 'trading', api_key=telegram_key, chat_id=telegram_chat)
-            else:
-                P.logger.debug("Telegram 알림 비활성화 상태거나 설정값이 없습니다. 메시지를 전송하지 않습니다.")
+            # Telegram 알림
+            send_telegram_message(msg)
 
         except Exception as e:
             P.logger.error(f'Exception:{str(e)}')
             P.logger.error(traceback.format_exc())
-
-
