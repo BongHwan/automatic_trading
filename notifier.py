551ab60916c347d19341ed02e5a55bea9e08b6bb
CT_B <fafamans@gmail.com>
2025-10-29 12:54:27 +0900

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,7 +1,6 @@
 from plugin import P, ToolNotify
 
 def send_telegram_message(msg: str):
-    """Telegram 설정에 따라 메시지를 전송"""
     try:
         enabled = P.ModelSetting.get_bool('basic_telegram_enabled')
         api_key = P.ModelSetting.get('basic_telegram_api_key')
@@ -10,9 +9,8 @@ def send_telegram_message(msg: str):
         if not enabled:
             P.logger.debug("Telegram 알림 비활성화 상태. 메시지 전송 생략.")
             return False
-
         if not api_key or not chat_id:
-            P.logger.warning("Telegram API key 또는 chat_id 설정 없음.")
+            P.logger.warning("Telegram API key 또는 chat_id 미설정.")
             return False
 
         ToolNotify.send_message(msg, 'trading', api_key=api_key, chat_id=chat_id)
