8bb50a7d99d9a3eb605801bb8b40b44c8661c1d4
CT_B <fafamans@gmail.com>
2025-10-29 13:57:34 +0900

    Update mod_basic.py

--- mod_basic.py
+++ mod_basic.py
@@ -1,19 +1,16 @@
-# mod_basic.py
 import traceback, os, time
-from flask import render_template, jsonify, request
+from flask import render_template, jsonify
 from plugin import PluginModuleBase
 from .setup import *
 from .trading_engine import TradingEngine
 from .notifier import send_telegram_message
-from .model import ModelSetting
 
 class ModuleBasic(PluginModuleBase):
 
     def __init__(self, P):
         super().__init__(P, name='basic', first_menu='setting', scheduler_desc="자동 트레이딩")
-        
         self.db_default = {
-            'db_version': '1',
+            f'db_version': '1',
             f'{self.name}_auto_start': 'False',
             f'{self.name}_interval': '0 */1 * * *',
             f'{self.name}_trade_amount_percent': 10,
@@ -28,16 +25,14 @@ class ModuleBasic(PluginModuleBase):
         }
 
     def process_menu(self, sub, req):
-        """UI 렌더링 시 설정값과 스케줄러 상태 전달"""
         arg = P.ModelSetting.to_dict()
         if sub == 'setting':
-            # 스케줄러 상태 반영 (lotto 모듈처럼)
-            arg['is_include'] = F.scheduler.is_include(self.get_scheduler_name()) if 'F' in globals() else False
-            arg['is_running'] = F.scheduler.is_running(self.get_scheduler_name()) if 'F' in globals() else False
+            # 스케줄러 상태
+            arg['is_include'] = F.scheduler.is_include(self.get_scheduler_name())
+            arg['is_running'] = F.scheduler.is_running(self.get_scheduler_name())
         return render_template(f'{P.package_name}_{self.name}_{sub}.html', arg=arg)
 
     def process_command(self, command, arg1, arg2, arg3, req):
-        """UI에서 명령 처리 (테스트 트레이딩)"""
         ret = {'ret': 'success'}
         try:
             engine = TradingEngine(P)
@@ -45,22 +40,21 @@ class ModuleBasic(PluginModuleBase):
                 result = engine.test_trade()
                 ret['data'] = result
                 ret['modal'] = f"시뮬레이션 결과: {result}"
-            elif command == 'save_settings':
-                # 설정 저장 처리
-                for key, value in req.form.items():
-                    if key in ['scheduler', 'is_running']:
-                        continue
-                    ModelSetting.set(key, value)
-                ret['modal'] = "설정이 저장되었습니다."
+                ret['title'] = '테스트'
+            elif command == 'execute_trade':
+                result = engine.execute_trade()
+                ret['data'] = result
+                ret['modal'] = f"실행 결과: {result}"
+                ret['title'] = '실행'
         except Exception as e:
             ret['ret'] = 'fail'
             ret['modal'] = str(e)
+            ret['title'] = '에러'
             P.logger.error(f'Exception:{str(e)}')
             P.logger.error(traceback.format_exc())
         return jsonify(ret)
 
     def scheduler_function(self):
-        """자동 트레이딩 스케줄러 실행"""
         try:
             auto_start = P.ModelSetting.get_bool(f'{self.name}_auto_start')
             if not auto_start:
