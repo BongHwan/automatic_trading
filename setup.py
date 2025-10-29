a69602dd90d274659684cf0472bd11b28ebe8ee8
CT_B <fafamans@gmail.com>
2025-10-29 13:48:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setting = {
         'list': [
             {'uri': 'setting', 'name': '설정'},
             {'uri': 'history', 'name': '거래 기록'},
-#            {'uri': 'manual', 'name': '매뉴얼', 'list': [{'uri': 'README.md', 'name': 'README.md'}]},
+            {'uri': 'manual', 'name': '매뉴얼', 'list': [{'uri': 'README.md', 'name': 'README.md'}]},
             {'uri': 'log', 'name': '로그'},
         ]
     },
@@ -34,5 +34,6 @@ try:
 except Exception as e:
     P.logger.error(f'Exception:{str(e)}')
     P.logger.error(traceback.format_exc())
+
 
 
