0ee4f0da518660bc8dbd998556489c6e654a67b5
CT_B <fafamans@gmail.com>
2025-10-29 16:08:29 +0900

    Update mod_main.py

--- mod_main.py
+++ mod_main.py
@@ -1,33 +1,25 @@
 from .setup import *
 
-name = 'main'
-
 class ModuleMain(PluginModuleBase):
 
     def __init__(self, P):
-        super(ModuleMain, self).__init__(P, name=name)
-        default_route_socketio_module(self)
-        # 화면/소켓용 더미 데이터
-        self.trade_data = {
-            "positions": [],
-            "orders": [],
-            "account": {"balance": 1000000, "equity": 1000000}
-        }
+        super(ModuleBasic, self).__init__(P, name='main', first_menu='setting', scheduler_desc="코인 자동 매매")
+        self.db_default = {
+            f'db_version': '1',
+            f'{self.name}_auto_start': 'False',
+            f'{self.name}_interval': '0 8 * * *',
+            f'{self.name}_db_delete_day': '30',
+            f'{self.name}_db_auto_delete': 'False',
+            f'{P.package_name}_item_last_list_option': '',
+        }        
 
-    def process_menu(self, page, req):
-        arg = {}
-        if hasattr(P, 'ModelSetting') and P.ModelSetting is not None:
-            arg = P.ModelSetting.to_dict()
-
-        # page가 None이면 기본 페이지로 변경
-        if page is None:
-            page = "setting"  # 기본 화면 html 이름
-
-        try:
-            return render_template(f'{self.P.package_name}_{self.name}_{page}.html', arg=arg)
-        except Exception:
-            return render_template("sample.html", title=f"{self.P.package_name} - {page}")
-
+    def process_menu(self, sub, req):
+        arg = P.ModelSetting.to_dict()
+        if sub == 'setting':
+            arg['is_include'] = F.scheduler.is_include(self.get_scheduler_name())
+            arg['is_running'] = F.scheduler.is_running(self.get_scheduler_name())
+        return render_template(f'{P.package_name}_{self.name}_{sub}.html', arg=arg)
+    
     def process_command(self, command, arg1=None, arg2=None, arg3=None, req=None):
         """
         버튼 클릭 등 명령 처리 (실제 기능 구현 없이 화면/소켓 테스트용)
@@ -50,3 +42,4 @@ class ModuleMain(PluginModuleBase):
         SocketIO로 현재 더미 데이터를 화면으로 전송
         """
         F.socketio.emit("status", self.trade_data, namespace=f'/{P.package_name}/{self.name}')
+
